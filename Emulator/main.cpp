621429bca45b1c908dc9de2c54024cee26c4bd60
MaximRicardo <maximricardo8@gmail.com>
2024-07-03 16:46:26 +0200

    Update main.cpp to use TTY instead of Bitmap

--- Emulator/main.cpp
+++ Emulator/main.cpp
@@ -1,5 +1,3 @@
-//Uses a 256 shade grayscale screen instead of a TTY
-
 #include <iostream>
 #include <memory>
 #include <cstring>
@@ -9,53 +7,92 @@
 #include "Input.hpp"
 
 
-#define Screen_Width 320
-#define Screen_Height 200
+#define SCR_WIDTH 1280
+#define SCR_HEIGHT 720
 
-#define Up_Screen_Width 1280
-#define Up_Screen_Height 800
+#define INPUT_ADDRESS 0x030000
+#define TTY_ADDRESS 0x020000
 
-#define SCR_ADDRS 0xff0000
-#define INPUT_ADDRS 0x030000
-#define TTY_ADDRS 0x020000
+#define IMPOSTOR2_CLOCK_SPEED (1.f/10000000.f)
 
-#define Impostor2_CLOCK_SPEED (1.f/10000000.f)
+#define CHAR_SIZE 20
+
+#define CHAR_RES_WIDTH SCR_WIDTH/CHAR_SIZE
+#define CHAR_RES_HEIGHT SCR_HEIGHT/CHAR_SIZE
 
 void ReadExeFile(unsigned char* io, char* fileName) {
 
 	FILE* file = fopen(fileName, "r");
 
-	size_t index = 0;
+    //Get the length of the file
+    fseek(file, 0L, SEEK_END);
+    size_t fileLength = ftell(file);
+    rewind(file);
 
-	//skip the first line
-	char c = 0;
-	while (c != '\n') c = fgetc(file);
+    //Allocate a string to store the file within
+    std::unique_ptr<char[]> fileStr = std::make_unique<char[]>(fileLength);
 
-	while (true) {
-		char line[2];
-		line[0] = fgetc(file);
-		if (line[0] == EOF) break;
-		line[1] = fgetc(file);
-		if (line[1] == EOF) break;
+    //Copy the file into the string
+    {
+        char c;
+        size_t i = 0;
+        while ((c = fgetc(file)) != EOF) fileStr[i++] = c;
+    }
 
-		unsigned char val = strtoul(line, NULL, 16);
+    //Close the file
+    fclose(file);
 
-        if (fgetc(file) == '*') {
+    //Split the string into lines
+    strtok(fileStr.get(), "\n");
 
-            //Skip val bytes
-            index += val;
+    //Skip the first line because it just contains information for logisim
+    char* line = strtok(NULL, "\n");
+
+    size_t currentAddress = 0;
+
+    while (line != NULL) {
+
+        //Get the number on the line
+        char* endPtr;
+        size_t num = strtoul(line, &endPtr, 16);
+
+        //Is there a multiplication after the number?
+        if (*endPtr == '*') {
+            //If so, skip num lines and fill them with the next number instead.
             
-            //Skip the other number
-            char c = 0;
-            while (c != '\n') c = fgetc(file);
+            //Move to the next character
+            ++endPtr;
+
+            //Read the number
+            unsigned char fillNum = strtoul(line, &endPtr, 16);
+
+            //Perform the fill
+            size_t endAddress = currentAddress + num;
+            for (; currentAddress < endAddress; currentAddress++) {
+                io[currentAddress] = fillNum;
+            }
         }
         else {
-		    io[index] = val;
-		    ++index;
+            //If not, write num to the current address, then move to the next byte
+            io[currentAddress] = num;
+            ++currentAddress;
         }
 
-		if (fgetc(file) == EOF) break; //Skip the newline char
-	}
+        //Now move to the next line
+        line = strtok(NULL, "\n");
+
+    }
+
+}
+
+void ShiftCharBufferUp(char* charBuffer) {
+
+    for (size_t y = 0; y < CHAR_RES_HEIGHT; y++) {
+        for (size_t x = 0; x < CHAR_RES_WIDTH; x++) {
+            if (y != CHAR_RES_HEIGHT-1) charBuffer[CHAR_RES_WIDTH*y+x] = charBuffer[CHAR_RES_WIDTH*(y+1)+x];
+            else charBuffer[CHAR_RES_WIDTH*y+x] = 0;
+        }
+    }
 
 }
 
@@ -66,17 +103,8 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-	InitWindow(Up_Screen_Width, Up_Screen_Height, "window");
+	InitWindow(SCR_WIDTH, SCR_HEIGHT, "window");
 	SetTargetFPS(60);
-
-	Image img = GenImageColor(Screen_Width, Screen_Height, BLACK);
-	ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
-	Texture tex = LoadTextureFromImage(img);
-
-	Rectangle sourceRec = { 0, (float)Screen_Height, (float)Screen_Width, (float)Screen_Height };
-	Rectangle destRec = { 0, 0, (float)Up_Screen_Width, (float)Up_Screen_Height};
-
-	std::unique_ptr<unsigned char[]> frameBuffer = std::make_unique<unsigned char[]>(Screen_Width * Screen_Height * 4);
 
 	Impostor2::Cpu processor;
 	for (int i = 0; i < 8; i++) processor.regs[i] = 0;
@@ -84,50 +112,80 @@ int main(int argc, char* argv[]) {
 
 	//16MB of available io
 	std::unique_ptr<unsigned char[]> io = std::make_unique<unsigned char[]>(16777216);
-	
 	memset(io.get(), 0, 16777216);
 
+    std::unique_ptr<char[]> charBuffer = std::make_unique<char[]>(CHAR_RES_WIDTH*CHAR_RES_HEIGHT);
+    memset(charBuffer.get(), 0, CHAR_RES_WIDTH*CHAR_RES_HEIGHT);
+
+    //Text cursor position
+    unsigned int cursorX = 0;
+    unsigned int cursorY = 0;
+	
 	ReadExeFile(io.get(), argv[1]);
 
 	while (!WindowShouldClose()) {
 		
 		float deltaTime = GetFrameTime();
 
-		char inputs[3];
-		Input::GetCharPressed(inputs);
-		io[INPUT_ADDRS] = inputs[0];
-		io[INPUT_ADDRS + 1] = inputs[1];
-		io[INPUT_ADDRS + 2] = inputs[2];
-
-		char key = GetCharPressed();
-		io[INPUT_ADDRS] = key;
+        char key = GetCharPressed();
+        if (IsKeyPressed(KEY_ENTER)) key = '\n';
+        else if (IsKeyPressed(KEY_BACKSPACE)) key = 8;
+        io[INPUT_ADDRESS] = key;
 
 		float totalCycles = 0.f;
-		while (totalCycles * Impostor2_CLOCK_SPEED < deltaTime) {
-			totalCycles += processor.ExecInstr(io.get());
-		}
+		while (totalCycles * IMPOSTOR2_CLOCK_SPEED < deltaTime) {
 
-		for (int y = 0; y < Screen_Height; y++)
-			for (int x = 0; x < Screen_Width; x++) {
-				unsigned int i = Screen_Width * y + x;
-				unsigned int address = (unsigned int)(io[SCR_ADDRS] << 16) + i;
-				frameBuffer[i * 4] = io[address];
-				frameBuffer[i * 4 + 1] = io[address];
-				frameBuffer[i * 4 + 2] = io[address];
-				frameBuffer[i * 4 + 3] = 255;
-			}
+            bool accessedInput;
+			totalCycles += processor.ExecInstr(io.get(), &accessedInput);
+
+            if (accessedInput) io[INPUT_ADDRESS] = 0;
+
+            if (io[TTY_ADDRESS] == '\n') {
+                ++cursorY;
+                cursorX = 0;
+
+                io[TTY_ADDRESS] = 0;
+            }
+            else if (io[TTY_ADDRESS] == 8) {
+                if (cursorX > 0)
+                    --cursorX;
+                else if (cursorY > 0) {
+                    cursorX = CHAR_RES_WIDTH-1;
+                    --cursorY;
+                }
+                charBuffer[CHAR_RES_WIDTH*cursorY+cursorX] = 0;
+
+                io[TTY_ADDRESS] = 0;
+            }
+            else if (io[TTY_ADDRESS] != 0) {
+                charBuffer[CHAR_RES_WIDTH*cursorY+cursorX] = io[TTY_ADDRESS];
+
+                if (cursorX < CHAR_RES_WIDTH-1) {
+                    ++cursorX;
+                }
+                else {
+                    cursorX = 0;
+                    if (cursorY < CHAR_RES_HEIGHT-1) ++cursorY;
+                    else ShiftCharBufferUp(charBuffer.get());
+                }
+
+                io[TTY_ADDRESS] = 0;
+            }
+		}
 
 		BeginDrawing();
 
-		UpdateTexture(tex, frameBuffer.get());
-		DrawTexturePro(tex, sourceRec, destRec, { 0.f, 0.f }, 0.f, WHITE);
+        ClearBackground(BLACK);
+
+        for (size_t y = 0; y < CHAR_RES_HEIGHT; y++) {
+            for (size_t x = 0; x < CHAR_RES_WIDTH; x++) {
+                DrawText(TextFormat("%c", charBuffer[CHAR_RES_WIDTH*y+x]), x * CHAR_SIZE, y * CHAR_SIZE, CHAR_SIZE, WHITE);
+            }
+        }
 
 		EndDrawing();
 
 	}
-
-	UnloadTexture(tex);
-	UnloadImage(img);
 
 	CloseWindow();
 
