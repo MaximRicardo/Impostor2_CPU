3d29c31f83c3d959ee1cee1b00260ef0e91da3e5
MaximRicardo <maximricardo8@gmail.com>
2024-07-03 16:52:57 +0200

    Update compile.sh

--- Emulator/compile.sh
+++ Emulator/compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 g++ -c Cpu.cpp -Wall -Wpedantic
-g++ -c Input.cpp -Wall -Wpedantic
 
-g++ main.cpp -o Emulator Cpu.o Input.o -Wall -Wpedantic -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+g++ main.cpp -o Emulator Cpu.o -Wall -Wpedantic -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
